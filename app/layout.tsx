803a6bb65c237d557770dff26de05d8bff44fe1b
julieyeeee <oopsyeh056@gmail.com>
2024-11-23 15:18:38 +0800

    feat: lesson 3: fonts and images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  /* antialiased 是 Tailwind 的樣式，對於字體起到修飾作用 */
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
